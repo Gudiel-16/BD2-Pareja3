f38d4e46b387ee5b6efe04ce5b144d573581110d
CHRISTTOPHER JOSE CHITAY COUTIÃ‘O <2142627070103@ingenieria.usac.edu.gt>
2023-12-19 07:48:54 -0800

    script update

--- practica2/scripts/tablas.sql
+++ practica2/scripts/tablas.sql
@@ -1,3 +1,5 @@
+USE bd2_practica1;
+
 CREATE TABLE registro_actividades (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(255),
@@ -9,5 +11,18 @@ CREATE TABLE registro_actividades (
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    rol VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE registros_respaldos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_archivo VARCHAR(255),
+    fecha_hora DATETIME
+);
+
+
+INSERT INTO usuarios(username, password, rol) values ('admin', '1234', 'administrador');
+
+SELECT * FROM usuarios;
+SELECT * FROM registros_respaldos;
