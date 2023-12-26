aab9880154c74eaf6585b3b6e7055b891b5bb22f
“Christopher <2834237140101@ingenieria.usac.edu.gt>
2023-12-26 00:17:39 -0600

    [CHANGE] Forma de guardar y obtener publicaciones de usuario

--- proyecto1/backend/controllers/publicacion.controller.js
+++ proyecto1/backend/controllers/publicacion.controller.js
@@ -16,18 +16,32 @@ const insertar_publicacion = async (req, res) => {
 const obtener_publicaciones_por_id_doctor = async (req, res) => {
     try {
 
+        let datos = {};
         let publicaciones = [];
+        let resultado_final = [];
 
         let results = await publicacionModel.obtenerPublicacionesPorIdDoctor(req.params);
 
         if(results.records.length > 0) {
-            // iterando e insertando publicaciones en array
-            results.records.forEach( (element) => {
-                publicaciones.push(element._fields[0].properties)
+
+            // asignando los datos de doctor
+            datos = results.records[0]._fields[0].properties;
+            // eliminando propiedad password del objeto
+            delete datos.password;
+
+            // iterando para separar el texto de la publicacion con la fecha
+            let pub = results.records[0]._fields[1].properties.pub;
+            pub.forEach( (element) => {
+                let partes = element.split("___"); // es el separador entre el texto y la fecha
+                // texto: uno todo lo del array excepto el ultimo elemento, fecha: siempre sera el ultimo elemento
+                publicaciones.push({texto: partes.slice(0, -1).join(''), fecha: partes[partes.length -1 ]});
             });
+
+            datos.publicaciones = publicaciones;
+            resultado_final.push(datos);
         }
 
-        response(res, 200, 'Publicaciones de doctor obtenidas con éxito', publicaciones);
+        response(res, 200, 'Publicaciones de doctor obtenidas con éxito', resultado_final);
 
     } catch (error) {
         return response(res, 400, 'Error al obtener publicaciones.', [error]);
--- proyecto1/backend/models/publicacion.model.js
+++ proyecto1/backend/models/publicacion.model.js
@@ -11,11 +11,14 @@ const insertarPublicacion = (params) => {
             const driver = createDriver();
             session = driver.session({ database: process.env.DB_NEO4J_NAME_DB });
 
-            // guardar doctor
-            const result = await session.run(
-                'CREATE (p:Publicacion {id_doctor: $id_doctor, texto: $texto, fecha: $fecha}) RETURN p',
-                params
-            );
+            // MERGE: si no existe el nodo Publicacion para un doctor, lo crea automaticamente 
+            // coalesce: si es null asigna un array vacio
+            let texto_y_fecha = params.texto + "___" + params.fecha;
+            let query = `MERGE (p:Publicacion {id_doctor: $id_doctor})          
+                        SET p.pub = coalesce(p.pub, []) + $new_pub
+                        RETURN p`;
+
+            const result = await session.run(query, {id_doctor: params.id_doctor, new_pub: texto_y_fecha});
 
             resolve(result);
 
@@ -44,7 +47,9 @@ const obtenerPublicacionesPorIdDoctor = (params) => {
 
             // guardar doctor
             const result = await session.run(
-                'MATCH (p:Publicacion {id_doctor: $id_doctor}) RETURN p',
+                `MATCH (u:Usuario {id_doctor: $id_doctor}) 
+                MATCH (p:Publicacion {id_doctor: $id_doctor}) 
+                RETURN u, p`,
                 params
             );
 
